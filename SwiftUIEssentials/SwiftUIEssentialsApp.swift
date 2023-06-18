2234c989ebd380978fd17ac193c2013c16e9adbc
RobJWong <RobertJWong246@gmail.com>
2023-06-18 17:20:37 -0400

    init commit

---
+++ SwiftUIEssentials/SwiftUIEssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssentialsApp.swift
+//  SwiftUIEssentials
+//
+//  Created by Robert Wong on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
